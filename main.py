a4835cbc8a5a49483ecf0509f0482def165cd56e
janeturovskaya <jane.turovskaya@gmail.com>
2025-07-20 20:11:58 +0300

    Added tick

--- main.py
+++ main.py
@@ -7,19 +7,19 @@ from constants import (SCREEN_WIDTH,
                        ASTEROID_MAX_RADIUS)
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+
         screen.fill(color='black')
         pygame.display.flip()
+        dt = time.tick(60) / 1000
 
 
 if __name__ == "__main__":
