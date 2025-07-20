92b68159047cf841199790670a61cbd6f9d93884
janeturovskaya <jane.turovskaya@gmail.com>
2025-07-20 22:17:15 +0300

    Added groups and movement

--- main.py
+++ main.py
@@ -5,11 +5,20 @@ from constants import (SCREEN_WIDTH,
                        ASTEROID_KINDS,
                        ASTEROID_SPAWN_RATE,
                        ASTEROID_MAX_RADIUS)
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     time = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    
     dt = 0
 
     while True:
@@ -17,9 +26,16 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
         screen.fill(color='black')
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = time.tick(60) / 1000
+
+
 
 
 if __name__ == "__main__":
